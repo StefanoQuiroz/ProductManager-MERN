4e04a8dea9d44bf692a0572e9eb7912f543695f0
Stefano <piero.quiroz93@gmail.com>
2021-07-14 11:32:44 -0500

    Center-Server configurations 50%

---
+++ class/server/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+//connect mongoDB with Mongoose
+const connectDB = require('./config/animals.config');
+//connectDB()
+
+//middlewares
+app.use(cors());
+app.use(express());
+app.use(express.urlencoded({extended:true}));
+
+//routes
+//app.use('/api', require('./routes/animals.routes'));
+
+
+app.listen(PORT, ()=>{
+    console.log(` 1 : Server Lock and Loading on PORT: ${PORT} `);
+})
