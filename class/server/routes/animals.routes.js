1e8d1c4a259ae5a384f5d56c89760056ce5e66ee
Stefano <piero.quiroz93@gmail.com>
2021-07-14 12:02:02 -0500

    Animal Routes finished

--- class/server/routes/animals.routes.js
+++ class/server/routes/animals.routes.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const router = express();
-const 
+const {findAnimal, findSingleAnimal, newAnimal, updateAnimal, deleteAnimal} = require('../controllers/animal.controllers');
+
+router.get('/animales', findAnimal);
+router.get('/animales/:id', findSingleAnimal);
+router.post('/animales/new', newAnimal);
+router.put('/animales/update/:id', updateAnimal);
+router.delete('/animales/delete/:id', deleteAnimal);
+
+module.exports = router;
