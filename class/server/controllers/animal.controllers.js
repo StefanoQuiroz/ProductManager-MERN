87adfa4aeeb4cabe4641182b969443198905aa95
Stefano <piero.quiroz93@gmail.com>
2021-07-14 17:41:02 -0500

    Animal Controllers finished

--- class/server/controllers/animal.controllers.js
+++ class/server/controllers/animal.controllers.js
@@ -1,46 +1,50 @@
 const Animal = require('../models/animals.model');
 
 const findAnimal = (req,res) => {
-    Animal.find({})
-        .then(result => res.json(result))
+    const filter = {};
+    if(req.query.nombre){
+        filter.nombre = req.query.nombre;
+    }
+    Animal.find(filter)
+        .then(result => res.json({data:result}))
         .catch(error => {
-            res.json(error);
+            res.json({error:error, message:"Something went wrong"});
             res.sendStatus(404)
         })
 }
 
 const findSingleAnimal = (req,res) => {
     Animal.findById(req.params.id)
-        .then(result => res.json(result))
+        .then(result => res.json({data:result}))
         .catch(error => {
-            res.json(error);
+            res.json({error:error, message:"Something went wrong"});
             res.sendStatus(404)
         })
 }
 
 const newAnimal = (req,res) => {
     Animal.create(req.body)
-        .then(result => res.json(result))
+        .then(result => res.json({data:result}))
         .catch(error => {
-            res.json(error);
+            res.json({error:error, message:"Something went wrong"});
             res.sendStatus(500)
         })
 }
 
 const updateAnimal = (req,res) => {
     Animal.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
-        .then(result => res.json(result))
+        .then(result => res.json({data:result}))
         .catch(error => {
-            res.json(error);
+            res.json({error:error, message:"Something went wrong"});
             res.sendStatus(500);
         })
 }
 
 const deleteAnimal = (req,res) => {
     Animal.deleteOne({_id:req.params.id})
-        .then(result => res.json(result))
+        .then(result => res.json({data:result}))
         .catch(error => {
-            res.json(error);
+            res.json({error:error, message:"Something went wrong"});
             res.sendStatus(202);
         })
 }
