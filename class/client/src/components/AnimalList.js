b50954a5a59807dd9d39e86b5d41ff594fa411b7
Stefano <piero.quiroz93@gmail.com>
2021-07-14 18:20:53 -0500

    Initial Components

---
+++ class/client/src/components/AnimalList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnimalList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AnimalList;
---
+++ class/client/src/components/AnimalManager.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnimalManager = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AnimalManager;
