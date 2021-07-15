0a1a41ead6ea20a35975a564e9fd29bd8b5d516e
Stefano <piero.quiroz93@gmail.com>
2021-07-15 11:19:41 -0500

    AnimalManager modified

--- class/client/src/components/AnimalManager.js
+++ class/client/src/components/AnimalManager.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import AnimalList from './AnimalList';
+import { Row } from 'reactstrap';
 
 const AnimalManager = () => {
-    return (
-        <div>
-            
-        </div>
+    const [datos, setDatos] = useState([]);
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/animales")
+        .then(response => setDatos(response.data.data))
+        .catch(err => Swal.fire({
+            icon: 'error',
+            title: 'Error al obtener los datos',
+            text: 'Ocurrió un problema al ntentar obtener el listado de animales'
+        }));
+    }, [])
+
+     return (
+        <Row>
+            <AnimalList animales={datos}/>
+        </Row>
     );
 }
 
