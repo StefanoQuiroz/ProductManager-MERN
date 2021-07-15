084bbbf0ce631fc44ddd4243b7035c026d56b4ec
Stefano <piero.quiroz93@gmail.com>
2021-07-15 11:17:58 -0500

    Added components

--- class/client/src/App.js
+++ class/client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import AnimalManager from './components/AnimalManager';
+import { Container } from 'reactstrap';
+import AnimalForm from './components/AnimalForm';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container>
+      <AnimalManager/>
+      <AnimalForm/>
+    </Container>
   );
 }
 
