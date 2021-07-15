838619665db17b858e81fb9240869bb0cf067b56
Stefano <piero.quiroz93@gmail.com>
2021-07-15 11:19:04 -0500

    AnimalList modified

--- class/client/src/components/AnimalList.js
+++ class/client/src/components/AnimalList.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Table, Col } from 'reactstrap';
 
-const AnimalList = () => {
+const AnimalList = (props) => {
+    const {animales} = props;
     return (
-        <div>
-            
-        </div>
+        <Col>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Acciones</th>
+                        <th>Nombre</th>
+                        <th>Tipo</th>
+                        <th>Color</th>
+                        <th>Tamaño</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {animales && animales.map((items, index) => (
+                        <tr key={index}>
+                            <td>&nbsp;</td>
+                            <td>{items.nombre}</td>
+                            <td>{items.tipo}</td>
+                            {/* <td>{items.tipo.codigo}</td> */}
+                            <td>{items.color}</td>
+                            <td>{items.tamanho}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>            
+        </Col>
     );
 }
 
