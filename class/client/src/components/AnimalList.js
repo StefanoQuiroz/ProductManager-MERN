ae606041cbf693a9d0c9172f376946a3c886d3cc
Stefano <piero.quiroz93@gmail.com>
2021-07-16 13:24:58 -0500

    Animal List modified

--- class/client/src/components/AnimalList.js
+++ class/client/src/components/AnimalList.js
@@ -3,9 +3,11 @@ import { useHistory } from 'react-router-dom';
 import { Table, Row, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faTrash, faPen} from '@fortawesome/free-solid-svg-icons';
+import Swal from 'sweetalert2';
+import axios from 'axios';
 
 const AnimalList = (props) => {
-    const {animales} = props;
+    const { datos, setDatos} = props;
     const history = useHistory()
     const ver = (event, id) => {
         history.push(`/ver/${id}`)
@@ -13,6 +15,28 @@ const AnimalList = (props) => {
 
     const modificar = (event, id) => {
         history.push(`/modificar/${id}`);
+    }
+
+    const borrar = (event, id) => {
+        Swal.fire({
+            icon: "warning",
+            title: "Eminar animal",
+            text: "¿Esta seguro quedesea eliminar el animal?",
+            showCancelButton: true
+        }).then(result => {
+            if(result.value){
+                axios.delete(` http://localhost:8000/api/animales/delete/${id}`)
+                    .then(response => {
+                        const deleteData = datos.filter(response => response._id !== id);
+                        setDatos(deleteData);
+                    })
+                    .catch(err => Swal.fire({
+                        icon: "error",
+                        title: "error",
+                        text: "Error al eliminar el animal"
+                    }))
+            }
+        })
     }
     
     return (
@@ -28,11 +52,12 @@ const AnimalList = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {animales && animales.map((items, index) => (
+                    {datos && datos.map((items, index) => (
                         <tr key={index}>
                             <td>
                                 <Button color="primary" style={{margin:'2px'}} onClick={(event) => ver(event, items._id)}><FontAwesomeIcon icon={faEye}/> Ver</Button>
                                 <Button color="secondary" style={{margin:'2px'}} onClick={(event) => modificar(event, items._id)}><FontAwesomeIcon icon={faPen}/> Editar</Button>
+                                <Button color="danger" style={{margin:'2px'}} onClick={(event) => borrar(event, items._id)}><FontAwesomeIcon icon={faTrash}/></Button>
                             </td>
                             <td>{items.nombre}</td>
                             <td>{items.tipo}</td>
