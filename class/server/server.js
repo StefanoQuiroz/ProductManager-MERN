82d5b9d771c527362220213e9541e740bce8f9a5
Stefano <piero.quiroz93@gmail.com>
2021-07-16 20:17:04 -0500

    Server modified out the .env

--- class/server/server.js
+++ class/server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 //connect mongoDB with Mongoose
 const connectDB = require('./config/animals.config');
